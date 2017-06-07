d30342d106272d6871d97bec0466b8e231fa8f94
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-06-07 18:44:39 +0300

    Global Refactoring

--- Protocol.Interfaces/Workers/Servers/IFlowServerWorker.cs
+++ Protocol.Interfaces/Workers/Servers/IFlowServerWorker.cs
@@ -3,8 +3,5 @@
     using Common;
 
     /* TCP/UDP Server */
-    public interface IFlowServerWorker : IFlowProtocol
-    {
-        //void ExecuteRequest(string request);
-    }
+    public interface IFlowServerWorker : IFlowProtocol { }
 }
